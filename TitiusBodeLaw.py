b0442dba1182360bb5815dda308222688f90b5da
Samriddhi <ssmaity94@gmail.com>
2018-04-17 13:56:26 +0530

    Create TitiusBodeLaw.py

---
+++ TitiusBodeLaw.py
@@ -0,0 +1,25 @@
+from numpy import *
+import scipy.optimize as op
+import matplotlib.pyplot as plt
+
+data = loadtxt('planetdistance.dat')
+n = data[:,0]
+a = data[:,1]
+
+def TitiusBode(n, p, q):
+  return p+q*2**n
+  
+seed = [0.0, 0.0]
+
+par, cov = op.curve_fit(TitiusBode, n, a, seed)
+p, q = par[0], par[1]
+print p, q
+
+plt.scatter(n, TitiusBode(n, p, q))
+plt.xlim(0.5, 10.0)
+plt.ylim(0.0, 50.0)
+plt.xlabel('planet index (n)')
+plt.ylabel('distance from Sun (AU)')
+plt.title('Modified Titius-Bode Law')
+plt.ticks(arange(0.5, 10.0, 1.0))
+plt.show()
