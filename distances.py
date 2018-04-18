c2e6e7c48f4555beedd28b979e46b29096f55ddf
Samriddhi <ssmaity94@gmail.com>
2018-04-18 15:58:21 +0530

    Update distances.py

--- distances.py
+++ distances.py
@@ -8,21 +8,20 @@ a = data[:,1]
 
 x = arange(0.0, 10.0, 0.10)
 
-def TitiusBode(n, p, q):
+def distance(n, p, q):
   return p + q*2**n
   
 seed = [0.0, 0.0]
 
-par, cov = op.curve_fit(TitiusBode, n, a, seed)
+par, cov = op.curve_fit(distance, n, a, seed)
 p, q = par[0], par[1]
 print p, q
 
-plt.scatter(n, TitiusBode(n, p, q))
-plt.plot(x, TitiusBode(x, p, q), 'r-')
+plt.scatter(n, distance(n, p, q))
+plt.plot(x,distance(x, p, q), 'r-')
 plt.xlim(0.5, 10.0)
 plt.ylim(0.0, 50.0)
 plt.xlabel('planet index (n)')
 plt.ylabel('distance from Sun (AU)')
-plt.title('Modified Titius-Bode Law')
 plt.ticks(arange(0.5, 10.0, 1.0))
 plt.show()
